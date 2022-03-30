61ac0980766ee24b0c9b96c0064e5efaa9bc00da
Aram <Txarm99@gmail.com>
2022-03-30 19:48:22 +0300

    task 1 done

--- src/ru/first/Main.java
+++ src/ru/first/Main.java
@@ -3,6 +3,14 @@ package ru.first;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	byte a = 1;
+    short b = 2;
+    int c = 3;
+    long d = 4L;
+    float e = 2.5f;
+    double f = 65.456;
+    boolean g = a < c;
+    char h = 63;
+    System.out.println(g);
     }
 }
